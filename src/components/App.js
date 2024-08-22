21b3f55a976bcc92ff23353e0296e63601b91147
CarlosGonzález20 <ll3224645@gmail.com>
2024-08-22 09:58:16 -0600

    eliminando contenido de state en App.js

--- src/components/App.js
+++ src/components/App.js
@@ -5,23 +5,7 @@ import Add from "./add";
 class App extends react.Component {
 
     state = {
-        todos: [
-            {
-                id: 1,
-                title: "Todo 1",
-                completed: false
-            },
-            {
-                id: 2,
-                title: "Todo 2",
-                completed: false
-            },
-            {
-                id: 3,
-                title: "Todo 3",
-                completed: false
-            }
-        ]
+        todos: []
     };
 
     render() {
